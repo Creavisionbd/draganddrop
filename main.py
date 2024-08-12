0722663475619741b320ce0af9d8583f4f2a1da3
Creavisionbd <142791531+Creavisionbd@users.noreply.github.com>
2024-08-12 15:52:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ feed.add_output()
 
 result = Block(name='Result')
 result.add_output()
-
-st_barfi(base_blocks=[feed, result])
+splitter = Block(name='Splitter')
+splitter.add_input()
+splitter.add_output()
+mixer = Block(name='Mixer')
+mixer.add_input()
+mixer.add_input()
+mixer.add_output()
+st_barfi(base_blocks=[feed, result,splitter,mixer])
