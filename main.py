2508fd47f3d35498f66aac2d3d01be1218d61d2a
Creavisionbd <142791531+Creavisionbd@users.noreply.github.com>
2024-08-12 13:44:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from barfi import st_barfi, barfi_schemas
-import streamlit as st
-from test_blocks import process_blocks
+from barfi import st_barfi, Block
 
-barfi_schema_name = st.selectbox(
-    'Select a saved schema to load:', barfi_schemas())
+feed = Block(name='Feed')
+feed.add_output()
 
-compute_engine = st.checkbox('Activate barfi compute engine', value=False)
+result = Block(name='Result')
+result.add_output()
 
-barfi_result = st_barfi(
-    base_blocks=process_blocks, compute_engine=compute_engine, load_schema=barfi_schema_name)
-
-if barfi_result:
-    st.write(barfi_result['Result-id-524173']['block'].get_interface(name='Input 1'))
-    st.write(barfi_result)
+st_barfi(base_blocks=[feed, result])
